3309a1a835ef01790b5c035f6a668e2a58eff22b
LyovMkhyan <levonnoy@gmail.com>
2023-02-13 19:12:22 +0400

    uppdate is_leap.py

--- is_leap.py
+++ is_leap.py
@@ -1,11 +1,10 @@
 date = int(input('Enter date: '))
 
-if date % 4 == 0:
-    if not date % 100 == 0:
-        print('Yes')
-    elif date % 100 == 0 and date % 400 == 0:
-        print('Yes')
-    else:
-        print('No')
+if date % 4 != 0:
+    print('No')
+elif date % 100 != 0:
+    print('Yes')
+elif date % 400 == 0:
+    print('Yes')
 else:
     print('No')
