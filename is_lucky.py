439e1544a09c9113a995990d45db52728c7022a0
LyovMkhyan <levonnoy@gmail.com>
2023-02-19 16:58:49 +0400

    add function lucky_number

--- is_lucky.py
+++ is_lucky.py
@@ -1,13 +1,10 @@
-number = input('Enter a number: ')
-half = len(number) // 2
-sum1,sum2 = 0,0 
-for index in range(len(number)):
-    if index < half:
-        sum1 += int(number[index])
-    else:
-        sum2 += int(number[index])
+def lucky_number(number: str) -> bool:
+    half = len(number) // 2
+    sum1, sum2 = 0, 0
+    for index in range(len(number)):
+        if index < half:
+            sum1 += int(number[index])
+        else:
+            sum2 += int(number[index])
 
-if sum1 == sum2 :
-    print("Yes")
-else:
-    print("No")
+    return sum1 == sum2
