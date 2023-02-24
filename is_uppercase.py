52f5568ce3beb110f196d0b2b47fd42ab44c159a
LyovMkhyan <levonnoy@gmail.com>
2023-02-24 19:12:34 +0400

    Created is_uppercase.py

---
+++ is_uppercase.py
@@ -0,0 +1,13 @@
+def is_upper(text: str) -> str:
+    if text == text.upper():
+        print("Yes")
+        return "Yes"
+    else:
+        print("No")
+        return 'No'
+
+
+is_upper('Jhon')
+is_upper("123")
+is_upper('')
+is_upper("ZINA")
